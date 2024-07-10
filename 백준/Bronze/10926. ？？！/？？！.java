ac45b22e6ac293c03e3cf2303712b9a97f8e630a
JunWooP0 <163957128+JunWooP0@users.noreply.github.com>
2024-07-10 16:33:30 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String line = name + "??!";
+        System.out.println(line);
+    }
+}
